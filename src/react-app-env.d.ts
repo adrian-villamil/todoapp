baddacced249490be6fcf0981e7d9ac7a103997a
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-09-11 10:24:40 -0500

    Added types for my todo object and my app context.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="react-scripts" />
+
+interface Todo {
+  id: string;
+  details: string;
+  completed: boolean;
+}
+
+interface TodoAppContext {
+  todos: Todo[];
+  addTodo: (todoDetails: string) => void;
+  toggleTodoStatus: (todoId: string, status: boolean) => void;
+  deleteOneTodo: (todoId: string) => void;
+  deleteCompletedTodos: () => void;
+  getActiveTodos: () => Todo[];
+  getCompletedTodos: () => Todo[];
+}
