34384d075df5376a8018b2189c4c402cc424d7c9
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-09-11 10:29:47 -0500

    Rendering only active todos.

--- src/pages/ActiveTodos/index.tsx
+++ src/pages/ActiveTodos/index.tsx
@@ -1,15 +1,19 @@
+import React from 'react';
 import Stack from '@mui/material/Stack';
 import { TodoItem } from "../../components/TodoItem";
 import { TodoForm } from "../../components/TodoForm";
+import { AppContext } from '../../context/AppContext';
 
 export const ActiveTodos: React.FC = () => {
+  const { getActiveTodos } = React.useContext(AppContext) as TodoAppContext;
+  
   return (
     <Stack spacing={2} my={'20px'}>
       <TodoForm />
       <Stack spacing={2}>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {getActiveTodos().map((todo) => {
+          return <TodoItem key={todo.id} todo={todo} />;
+        })}
       </Stack>
     </Stack>
   );
