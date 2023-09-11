b307a7752329ba45edd7f54febc97f9fc3a57feb
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-09-10 22:12:03 -0500

    Added tabs to display three different contents.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { styled } from '@mui/material/styles';
+import { AllTodos } from './pages/AllTodos';
+import { ActiveTodos } from './pages/ActiveTodos';
+import { CompletedTodos } from './pages/CompletedTodos';
 import './App.css';
 
-function App() {
+const TabContent = styled(TabPanel)(({ theme }) => ({
+  padding: '0',
+}));
+
+const TabListStyles = {
+  "& .MuiTabs-flexContainer": {
+    justifyContent: 'space-around'
+  },
+  '& .MuiButtonBase-root': {
+    fontFamily: 'Montserrat',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+    color: '#333333'
+  },
+  '& .MuiTabs-indicator': {
+    height: '4px',
+    borderRadius: '4px 4px 0 0',
+    backgroundColor: '#2F80ED'
+  }
+};
+
+export const App: React.FC = () => {
+  const [positionTab, setPositionTab] = useState<string>('1');
+
+  const handleTabChange = (event: React.SyntheticEvent, value: string): void => {
+    setPositionTab(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* TITLE */}
+      <Typography
+        sx={{
+          textAlign: 'center',
+          fontFamily: 'Raleway',
+          fontWeight: '700',
+          fontSize: '36px',
+          color: '#333333',
+          padding: '26px',
+          letterSpacing: '-1.62px'
+        }}
+      >
+        #todo
+      </Typography>
+
+      {/* TABS */}
+      <Box sx={{ width: '100%', typography: 'body1' }}>
+        <TabContext value={positionTab}>
+          <Box sx={{ borderBottom: 1, borderColor: '#bdbdbd' }}>
+            <TabList
+              onChange={handleTabChange}
+              sx={TabListStyles}
+            >
+              <Tab label='All' value='1' />
+              <Tab label='Active' value='2' />
+              <Tab label='Completed' value='3' />
+            </TabList>
+          </Box>
+          <TabContent value='1'>
+            <AllTodos />
+          </TabContent>
+          <TabContent value='2'>
+            <ActiveTodos />
+          </TabContent>
+          <TabContent value='3'>
+            <CompletedTodos />
+          </TabContent>
+        </TabContext>
+      </Box>
     </div>
   );
-}
-
-export default App;
+};
