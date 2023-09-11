bc35bc546362e08a0d5f74cebc8e83aa36151ca2
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-09-11 10:28:24 -0500

    Rendering all todos.

--- src/pages/AllTodos/index.tsx
+++ src/pages/AllTodos/index.tsx
@@ -1,15 +1,17 @@
+import React from 'react';
 import Stack from '@mui/material/Stack';
 import { TodoItem } from '../../components/TodoItem';
 import { TodoForm } from '../../components/TodoForm';
+import { AppContext } from '../../context/AppContext';
 
 export const AllTodos: React.FC = () => {
+  const { todos } = React.useContext(AppContext) as TodoAppContext;
+
   return (
     <Stack spacing={2} my={'20px'}>
       <TodoForm />
       <Stack spacing={2}>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)}
       </Stack>
     </Stack>
   );
